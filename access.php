b82645be476789e05667db3594c61c36a6f92d03
Jared Zheng <Jared.alas.zheng@gmail.com>
2022-09-01 10:22:50 -0400

    attempt to update database

--- access.php
+++ access.php
@@ -1,9 +1,9 @@
 <?php
 
 $server = "localhost";
-$username = "cpses_pbabrs8zha";
-$password = "phylliszhang";
-$database = "phyljqqr_CCit";
+$username = "pbhabths_user";
+$password = "IGclLqKtodF]";
+$database = "pbhabths_database";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 mysqli_set_charset('utf8', $conn);
