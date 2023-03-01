556db451be255705280f55531b2c7a96be1981e5
Jared Zheng <Jared.alas.zheng@gmail.com>
2023-02-28 20:39:21 -0500

    study materials

---
+++ nav.js
@@ -0,0 +1,34 @@
+document.write('<nav class="navbar fixed-top navbar-expand-lg navbar-dark fixed-top" style="background-color: #1d2ade;">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">PBHA Chinatown Citizenship</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="classes.html">Classes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="calendar.html">Calendar</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Sign Up
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a class="dropdown-item" href="learn.html">Learn</a>
+              <a class="dropdown-item" href="teach.html">Teach</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://resources.pbhachinatowncitizenship.com/">Resources</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+');
